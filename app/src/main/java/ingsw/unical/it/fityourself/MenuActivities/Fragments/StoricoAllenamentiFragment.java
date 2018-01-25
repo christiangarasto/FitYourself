a82deb4fcb798dd4b700b525f5b4ed4bcc381f83
Christian Garasto <christiangarasto@gmail.com>
2018-01-25 20:43:02 +0100

    Aggiunto calendario per la cronologia degli allenamenti, da implementare la logica Sessione di corsa completata al 90%. Rimane da fare: fragment per l visualizzazione degli obiettivi raggiunti e salvataggio dell'allenamento solo se si è raggiunto almeno un obiettivo finale

--- app/src/main/java/ingsw/unical/it/fityourself/MenuActivities/Fragments/StoricoAllenamentiFragment.java
+++ app/src/main/java/ingsw/unical/it/fityourself/MenuActivities/Fragments/StoricoAllenamentiFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ingsw.unical.it.fityourself.R;
 
@@ -14,21 +19,31 @@ import ingsw.unical.it.fityourself.R;
  */
 public class StoricoAllenamentiFragment extends Fragment implements GenericFragment{
 
+    View rootView;
+
+
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    String userId;
 
     public StoricoAllenamentiFragment() {
-        // Required empty public constructor
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("Allenamenti");
+        userId = FirebaseAuth.getInstance().getUid();
     }
-
+    //calendarioAllenamenti
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         getActivity().setTitle("Storico allenamenti");
+        rootView = inflater.inflate(R.layout.fragment_storico_allenamenti, container, false);
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_storico_allenamenti, container, false);
+        CalendarView cal = (CalendarView) rootView.findViewById(R.id.calendarioAllenamenti);
+
+
+        return rootView;
     }
 
     @Override
