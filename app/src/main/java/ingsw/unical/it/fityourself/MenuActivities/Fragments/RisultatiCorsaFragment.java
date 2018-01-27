3624017089759183ca2a16613011235bf05b26b8
AndrY11086 <andreapuleio86@gmail.com>
2018-01-27 16:26:19 +0100

    input inattesi: RISOLTO
    non si può utilizzare l'app senza inserire i dati personali: RISOLTO
    (Risolto = la schermata principale è ALLENATI ma se i dati non sono inseriti l'utente viene reindirizzato al fragment DatiPersonali)
    
    tutti i fragment sono singleton: RISOLTO
    
    fragment per i risultati della corsa: RISOLTO, manca solo da salvare l'allenamento

--- app/src/main/java/ingsw/unical/it/fityourself/MenuActivities/Fragments/RisultatiCorsaFragment.java
+++ app/src/main/java/ingsw/unical/it/fityourself/MenuActivities/Fragments/RisultatiCorsaFragment.java
@@ -1,12 +1,16 @@
 package ingsw.unical.it.fityourself.MenuActivities.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.Toast;
 
 import ingsw.unical.it.fityourself.R;
 
@@ -20,6 +24,7 @@ public class RisultatiCorsaFragment extends Fragment implements GenericFragment{
     private CheckedTextView nPassi;
     private CheckedTextView distanza;
     private CheckedTextView nCalorieBruciate;
+    private Button escibtn, salvabtn;
 
     private static RisultatiCorsaFragment risultatiCorsaFragment = null;
 
@@ -40,50 +45,91 @@ public class RisultatiCorsaFragment extends Fragment implements GenericFragment{
 
         rootView = inflater.inflate(R.layout.fragment_risultati_corsa, container, false);
 
+        escibtn = (Button) rootView.findViewById(R.id.esci);
+
         nPassi = (CheckedTextView) rootView.findViewById(R.id.passiEffettuati);
         distanza = (CheckedTextView) rootView.findViewById(R.id.distanzaPercorsa);
         nCalorieBruciate = (CheckedTextView) rootView.findViewById(R.id.calorieBruciate);
 
-        nPassi.setText(CorsaFragment.getPassiEffettuati().toString());
-        distanza.setText(CorsaFragment.getDistanzaPercorsa().toString());
-        nCalorieBruciate.setText(CorsaFragment.getCalorieBruciate().toString());
+        nPassi.setText(CorsaFragment.getPassiEffettuati().getText().toString());
+        distanza.setText(CorsaFragment.getDistanzaPercorsa().getText().toString());
+        nCalorieBruciate.setText(CorsaFragment.getCalorieBruciate().getText().toString());
 
-        int passi = Integer.parseInt(nPassi.toString());
-        int obPassi = Integer.parseInt(CorsaFragment.getObiettivoP().toString());
+        int passi = Integer.parseInt(nPassi.getText().toString());
+        int obPassi, obDist, obKcal;
 
-        int dist = Integer.parseInt(distanza.toString());
-        int obDist = Integer.parseInt(CorsaFragment.getObiettivoD().toString());
+        if(!CorsaFragment.getObiettivoP().getText().toString().equals(""))
+               obPassi = Integer.parseInt(CorsaFragment.getObiettivoP().getText().toString());
+        else
+            obPassi = 0;
 
-        int kCal = Integer.parseInt(nCalorieBruciate.toString());
-        int obKcal = Integer.parseInt(CorsaFragment.getObiettivoC().toString());
 
-        nPassi.setChecked(true);
-        distanza.setChecked(false);
+        int dist = Integer.parseInt(distanza.getText().toString());
+        if(!CorsaFragment.getObiettivoD().getText().toString().equals(""))
+            obDist = Integer.parseInt(CorsaFragment.getObiettivoD().getText().toString());
+        else
+            obDist = 0;
+
+        int kCal = Integer.parseInt(nCalorieBruciate.getText().toString());
+        if(!CorsaFragment.getObiettivoC().getText().toString().equals(""))
+            obKcal = Integer.parseInt(CorsaFragment.getObiettivoC().getText().toString());
+        else
+            obKcal = 0;
+
 
         if(CorsaFragment.getPassiEffettuati().length() > 0)
         {
             if(passi >= obPassi && obPassi > 0)
-                nPassi.setChecked(true);
+            {
+                nPassi.setCheckMarkDrawable(R.drawable.checked);
+                nPassi.setTextColor(Color.GREEN);
+            }
 
             else
-                nPassi.setChecked(false);
+            {
+                nPassi.setCheckMarkDrawable(R.drawable.notchecked);
+                nPassi.setTextColor(Color.RED);
+            }
         }
 
         if(CorsaFragment.getDistanzaPercorsa().length() > 0)
         {
             if(dist >= obDist && obDist > 0)
-                distanza.setChecked(true);
+            {
+                distanza.setCheckMarkDrawable(R.drawable.checked);
+                distanza.setTextColor(Color.GREEN);
+            }
             else
-                distanza.setChecked(false);
+            {
+                distanza.setCheckMarkDrawable(R.drawable.notchecked);
+                distanza.setTextColor(Color.RED);
+            }
         }
 
         if(CorsaFragment.getCalorieBruciate().length() > 0)
         {
             if( kCal >= obKcal && obKcal > 0)
-                nCalorieBruciate.setChecked(true);
+            {
+                nCalorieBruciate.setCheckMarkDrawable(R.drawable.checked);
+                nCalorieBruciate.setTextColor(Color.GREEN);
+            }
             else
-                nCalorieBruciate.setChecked(false);
+            {
+                nCalorieBruciate.setCheckMarkDrawable(R.drawable.notchecked);
+                nCalorieBruciate.setTextColor(Color.RED);
+            }
         }
+
+        escibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GenericFragment dati = AllenamentoFragment.getInstance();
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, dati.getFragment());
+                fragmentTransaction.commit();
+            }
+        });
 
         return rootView;
     }
